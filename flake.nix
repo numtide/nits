f0df80a32b7b8eddbb5e0205613c81506fabb609
Brian McGee <brian@bmcgee.ie>
2023-08-10 12:28:50 +0100

    feat: add garnix binary cache

--- flake.nix
+++ flake.nix
@@ -3,9 +3,11 @@
 
   nixConfig = {
     extra-substituters = [
+      "https://cache.garnix.io"
       "https://nix-community.cachix.org"
     ];
     extra-trusted-public-keys = [
+      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
