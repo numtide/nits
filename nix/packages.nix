d95b9657ce8279e3e35a7b597608d83345eba9b7
Brian McGee <brian@bmcgee.ie>
2023-08-10 12:29:54 +0100

    feat: add nsc and natscli runtime inputs for package build

--- nix/packages.nix
+++ nix/packages.nix
@@ -14,6 +14,8 @@
         pname = "nits";
         version = "0.0.1+dev";
 
+        runtimeInputs = with pkgs; [ nsc natscli ];
+
         src = lib.cleanSourceAndNix ../.;
         vendorSha256 = "sha256-y2jAASkdaDP+BzB/E+M3X12jSkFm5449eogHT1kGScs=";
 
