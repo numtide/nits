7c377f616b52ebee82522c3d2ac644b0f3236e9b
Brian McGee <brian@bmcgee.ie>
2023-09-13 09:24:31 +0100

    chore: remove binary cache component
    Better to implement it separately

--- internal/cmd/cmd.go
+++ internal/cmd/cmd.go
@@ -2,8 +2,6 @@ package cmd
 
 import (
 	"context"
-	"io"
-	"os"
 	"os/exec"
 	"syscall"
 	"time"
@@ -13,12 +11,7 @@ import (
 	nsccmd "github.com/nats-io/nsc/v2/cmd"
 	nexec "github.com/numtide/nits/pkg/exec"
 
-	"github.com/numtide/nits/pkg/cache"
-
-	"github.com/nix-community/go-nix/pkg/narinfo/signature"
-
 	"github.com/charmbracelet/log"
-	"github.com/numtide/nits/pkg/config"
 )
 
 type (
@@ -36,38 +29,6 @@ func (lo *LogOptions) ConfigureLog() {
 	log.SetLevel(log.ParseLevel(lo.Level))
 }
 
-type CacheOptions struct {
-	Subject        string   `env:"NITS_CACHE_SUBJECT" default:"NITS.CACHE"`
-	Group          string   `env:"NITS_CACHE_GROUP" default:"cache"`
-	StoreDir       string   `env:"NITS_CACHE_STORE_DIR" default:"/nix/store"`
-	WantMassQuery  bool     `env:"NITS_CACHE_WANT_MASS_QUERY" default:"true"`
-	Priority       int      `env:"NITS_CACHE_PRIORITY" default:"1"`
-	PrivateKeyFile *os.File `env:"NITS_CACHE_PRIVATE_KEY_FILE" required:""`
-}
-
-func (o *CacheOptions) ToCacheOptions() (*cache.Options, error) {
-	bytes, err := io.ReadAll(o.PrivateKeyFile)
-	if err != nil {
-		return nil, err
-	}
-
-	secretKey, err := signature.LoadSecretKey(string(bytes))
-	if err != nil {
-		return nil, err
-	}
-
-	return &cache.Options{
-		SecretKey: &secretKey,
-		Subject:   o.Subject,
-		Group:     o.Group,
-		Info: &cache.Info{
-			StoreDir:      o.StoreDir,
-			WantMassQuery: o.WantMassQuery,
-			Priority:      o.Priority,
-		},
-	}, nil
-}
-
 func Run(main func(ctx context.Context) error) (err error) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
@@ -76,24 +37,6 @@ func Run(main func(ctx context.Context) error) (err error) {
 	go shutdown.Listen(syscall.SIGINT, syscall.SIGTERM)
 
 	return main(ctx)
-}
-
-type CacheProxyOptions struct {
-	Subject   string `env:"NITS_CACHE_PROXY_SUBJECT" default:"NITS.CACHE"`
-	PublicKey string `env:"NITS_CACHE_PROXY_PUBLIC_KEY"`
-}
-
-func (c *CacheProxyOptions) ToCacheProxyConfig() (*config.CacheProxy, error) {
-	// todo validate format
-
-	if c.PublicKey == "" {
-		return nil, nil
-	}
-
-	return &config.CacheProxy{
-		Subject:   c.Subject,
-		PublicKey: c.PublicKey,
-	}, nil
 }
 
 func DetectOperator() (operator nsccmd.OperatorDescriber, err error) {
--- pkg/config/nats.go
+++
@@ -1,6 +0,0 @@
-package config
-
-type CacheProxy struct {
-	Subject   string
-	PublicKey string
-}
