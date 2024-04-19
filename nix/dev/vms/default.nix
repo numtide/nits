0209db65c63659f95fc34663e63017de85eccdc5
Brian McGee <brian@bmcgee.ie>
2024-04-19 12:21:23 +0100

    fix: run vms with bootloader
    Fixes #9
    
    Signed-off-by: Brian McGee <brian@bmcgee.ie>

--- nix/dev/vms/default.nix
+++ nix/dev/vms/default.nix
@@ -9,7 +9,6 @@
     pkgs,
     modules ? [
       ./modules/base.nix
-      ./modules/vm.nix
       ./modules/agent.nix
     ],
   }:
@@ -84,13 +83,7 @@ in {
           category = "development";
           help = "run an agent vm";
           name = "run-test-vm";
-          command = "nix run .#nixosConfigurations.${system}_test-vm-$1.config.system.build.vm";
-        }
-        {
-          category = "development";
-          help = "build an agent vm";
-          name = "build-test-vm";
-          command = "nix build .#nixosConfigurations.${system}_test-vm-$1.config.system.build.vm";
+          command = "nix run .#nixosConfigurations.${system}_test-vm-$1.config.system.build.vmWithBootLoader";
         }
       ];
     };
--- nix/dev/vms/modules/base.nix
+++ nix/dev/vms/modules/base.nix
@@ -2,19 +2,16 @@
   lib,
   config,
   pkgs,
+  id,
   hostname,
-  modulesPath,
   ...
 }: {
-  imports = ["${toString modulesPath}/virtualisation/qemu-vm.nix"];
-
   nix = {
     nixPath = [
       "nixpkgs=${pkgs.path}"
     ];
     settings = {
       experimental-features = "nix-command flakes";
-
       substituters = ["http://10.0.2.2:3000"];
       trusted-public-keys = [(lib.readFile ../../binary-cache/key.pub)];
     };
@@ -22,15 +19,66 @@
 
   networking.hostName = hostname;
   system.stateVersion = config.system.nixos.version;
-  boot.loader.grub.devices = lib.mkForce ["/dev/sda"];
-  fileSystems."/".device = lib.mkDefault "/dev/sda";
 
-  users.users.root.initialPassword = "";
+  boot = {
+    growPartition = true;
+    kernelParams = ["console=ttyS0"];
+    loader = {
+      timeout = 0;
+      grub.device = "/dev/vda";
+    };
+  };
 
-  services.openssh = {
-    enable = true;
-    settings = {
-      PermitRootLogin = "yes";
+  fileSystems."/" = {
+    device = "/dev/vda";
+    autoResize = true;
+    fsType = "ext4";
+  };
+
+  users.users.nits = {
+    isNormalUser = true;
+    extraGroups = ["wheel"];
+  };
+  services.getty.autologinUser = "nits";
+  security.sudo.wheelNeedsPassword = false;
+
+  services.openssh.enable = true;
+
+  virtualisation.vmVariantWithBootLoader = {
+    virtualisation = {
+      graphics = false;
+      diskSize = 5120;
+      diskImage = "$VM_DATA_DIR/${hostname}/disk.qcow2";
+
+      writableStoreUseTmpfs = false;
+
+      forwardPorts = [
+        {
+          from = "host";
+          # start at 2222 and increment
+          host.port = 2221 + id;
+          guest.port = 22;
+        }
+      ];
+
+      sharedDirectories = {
+        config = {
+          source = "$VM_DATA_DIR/${hostname}";
+          target = "/mnt/shared";
+        };
+      };
+    };
+
+    system.activationScripts = {
+      # replace host key with pre-generated one
+      host-key.text = ''
+        rm -f /etc/ssh/ssh_host_ed25519_key*
+        cp /mnt/shared/ssh_host_ed25519_key /etc/ssh/ssh_host_ed25519_key
+        cp /mnt/shared/ssh_host_ed25519_key.pub /etc/ssh/ssh_host_ed25519_key.pub
+
+        chmod 600 /etc/ssh/ssh_host_ed25519_key
+        chmod 644 /etc/ssh/ssh_host_ed25519_key.pub
+      '';
     };
   };
 }
