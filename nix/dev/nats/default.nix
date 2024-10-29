0872a3a419843986818c7863b01e57f6a5ab2c7b
kfollesdal <kristoffer.follesdal@eviny.no>
2024-10-29 21:36:28 +0100

    Move devshell definitions from nix/dev/nats/default.nix to nix/devshell.nix

--- nix/dev/nats/default.nix
+++ nix/dev/nats/default.nix
@@ -1,28 +1,5 @@
 {lib, ...}: {
-  perSystem = {pkgs, ...}: let
-    config = pkgs.writeTextFile {
-      name = "nats.conf";
-      text = ''
-        ## Default NATS server configuration (see: https://docs.nats.io/running-a-nats-service/configuration)
-
-        ## Host for client connections.
-        host: "127.0.0.1"
-
-        ## Port for client connections.
-        port: 4222
-
-        ## Port for monitoring
-        http_port: 8222
-
-        ## Configuration map for JetStream.
-        ## see: https://docs.nats.io/running-a-nats-service/configuration#jetstream
-        jetstream {}
-
-        # include paths must be relative so for simplicity we just read in the auth.conf file
-        include './auth.conf'
-      '';
-    };
-  in {
+  perSystem = {pkgs, ...}: {
     config.process-compose = {
       dev.settings.processes = {
         nats-server = {
@@ -67,75 +44,6 @@
           };
         };
       };
-    };
-
-    config.devshells.default = {
-      devshell.startup = {
-        setup-nats = {
-          deps = ["setup-test-vms"];
-          text = ''
-            set -euo pipefail
-
-            # we only setup the data dir if it doesn't exist
-            # to refresh simply delete the directory and run `direnv reload`
-            [ -d $NSC_HOME ] && exit 0
-
-            mkdir -p $NSC_HOME
-
-            # initialise nsc state
-
-            nsc init -n Nits --dir $NSC_HOME
-            nsc edit operator \
-                --service-url nats://localhost:4222 \
-                --account-jwt-server-url nats://localhost:4222
-
-            # setup server config
-
-            mkdir -p $NATS_HOME
-            cp ${config} "$NATS_HOME/nats.conf"
-            nsc generate config --nats-resolver --config-file "$NATS_HOME/auth.conf"
-          '';
-        };
-      };
-
-      env = [
-        {
-          name = "NATS_HOME";
-          eval = "$PRJ_DATA_DIR/nats";
-        }
-        {
-          name = "NSC_HOME";
-          eval = "$PRJ_DATA_DIR/nsc";
-        }
-        {
-          name = "NKEYS_PATH";
-          eval = "$NSC_HOME";
-        }
-        {
-          name = "NATS_JWT_DIR";
-          eval = "$PRJ_DATA_DIR/nats/jwt";
-        }
-      ];
-
-      packages = [
-        pkgs.nkeys
-        pkgs.nats-top
-      ];
-
-      commands = let
-        category = "nats";
-      in [
-        {
-          inherit category;
-          name = "nsc";
-          command = ''XDG_CONFIG_HOME=$PRJ_DATA_DIR ${pkgs.nsc}/bin/nsc -H "$NSC_HOME" "$@"'';
-        }
-        {
-          inherit category;
-          name = "nats";
-          command = ''XDG_CONFIG_HOME=$PRJ_DATA_DIR ${pkgs.natscli}/bin/nats "$@"'';
-        }
-      ];
     };
   };
 }
--- nix/devshell.nix
+++ nix/devshell.nix
@@ -20,6 +20,22 @@ in
         name = "VM_DATA_DIR";
         eval = "$PRJ_DATA_DIR/vms";
       }
+      {
+        name = "NATS_HOME";
+        eval = "$PRJ_DATA_DIR/nats";
+      }
+      {
+        name = "NSC_HOME";
+        eval = "$PRJ_DATA_DIR/nsc";
+      }
+      {
+        name = "NKEYS_PATH";
+        eval = "$NSC_HOME";
+      }
+      {
+        name = "NATS_JWT_DIR";
+        eval = "$PRJ_DATA_DIR/nats/jwt";
+      }
     ];
 
     devshell.startup = {
@@ -36,6 +52,55 @@ in
           ssh-keygen -t ed25519 -q -C root@test-vm-$i -N "" -f "$OUT/ssh_host_ed25519_key"
         done
       '';
+
+      setup-nats = let
+        config = pkgs.writeTextFile {
+          name = "nats.conf";
+          text = ''
+            ## Default NATS server configuration (see: https://docs.nats.io/running-a-nats-service/configuration)
+
+            ## Host for client connections.
+            host: "127.0.0.1"
+
+            ## Port for client connections.
+            port: 4222
+
+            ## Port for monitoring
+            http_port: 8222
+
+            ## Configuration map for JetStream.
+            ## see: https://docs.nats.io/running-a-nats-service/configuration#jetstream
+            jetstream {}
+
+            # include paths must be relative so for simplicity we just read in the auth.conf file
+            include './auth.conf'
+          '';
+        };
+      in {
+        deps = ["setup-test-vms"];
+        text = ''
+          set -euo pipefail
+
+          # we only setup the data dir if it doesn't exist
+          # to refresh simply delete the directory and run `direnv reload`
+          [ -d $NSC_HOME ] && exit 0
+
+          mkdir -p $NSC_HOME
+
+          # initialise nsc state
+
+          nsc init -n Nits --dir $NSC_HOME
+          nsc edit operator \
+              --service-url nats://localhost:4222 \
+              --account-jwt-server-url nats://localhost:4222
+
+          # setup server config
+
+          mkdir -p $NATS_HOME
+          cp ${config} "$NATS_HOME/nats.conf"
+          nsc generate config --nats-resolver --config-file "$NATS_HOME/auth.conf"
+        '';
+      };
     };
 
     packages = with lib;
@@ -48,6 +113,11 @@ in
           pkgs.golangci-lint
           pkgs.openssl
           pkgs.qemu-utils
+        ]
+        # nats
+        [
+          pkgs.nkeys
+          pkgs.nats-top
         ]
         # platform dependent CGO dependencies
         (mkIf isLinux [
@@ -109,6 +179,16 @@ in
         help = "run an agent vm";
         name = "run-test-vm";
         command = "nix run .#nixosConfigurations.${system}_test-vm-$1.config.system.build.vmWithBootLoader";
+      }
+      {
+        category = "nats";
+        name = "nsc";
+        command = ''XDG_CONFIG_HOME=$PRJ_DATA_DIR ${pkgs.nsc}/bin/nsc -H "$NSC_HOME" "$@"'';
+      }
+      {
+        category = "nats";
+        name = "nats";
+        command = ''XDG_CONFIG_HOME=$PRJ_DATA_DIR ${pkgs.natscli}/bin/nats "$@"'';
       }
     ];
   }
